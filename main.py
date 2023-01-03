f3d04248a8ad1619d82cc3881d9215cfcedf724d
Crazy-taji <51854770+Crazy-taji@users.noreply.github.com>
2023-01-04 03:05:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, Request
 import os
+import cv2
 
 app = Flask(__name__)
 
+number = 13
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/send_text')
+def send_text():
+    def generate():
+        global number
+        yield str(number)
+    return Response(generate(), mimetype='text')
 
 
 if __name__ == '__main__':
