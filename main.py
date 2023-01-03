0c0936cd7ba72599409fa229ae235087f343db4e
Crazy-taji <51854770+Crazy-taji@users.noreply.github.com>
2023-01-04 04:56:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ number = 13
 @app.route('/')
 def index():
     ret, __ = cv2.VideoCapture(1).read()
-    
-        if not ret:
-            return render_template("index.html", val = "ret not worked")
-        else:
-            return render_template("index.html", val = "ret worked")
+    if not ret:$
+        return render_template("index.html", val = "ret not worked")
+    else:
+        return render_template("index.html", val = "ret worked")
 
 
 if __name__ == '__main__':
